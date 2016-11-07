0232e45650a42294926090a8e2f6349f90dc7f03
rupinder83 <rupinder.nayyar@hotmail.com>
2016-11-07 10:26:21 +0100

    rename gbproject

---
+++ GBProjectTest/Driver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace GBProject
+{
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver();
+            Instance.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+
+        }
+
+        public static void Close()
+        {
+           Instance.Quit();
+        }
+    }
+}
---
+++ GBProjectTest/GBPage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace GBProject
+{
+    public class GBPage
+    {
+        public static void GoTo()
+        {
+            Driver.Instance.Navigate().GoToUrl("http://www.globalblue.com/customer-services/tax-free-shopping/refund-calculator/");
+           // var wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
+            //wait.Until((d => d.SwitchTo().ActiveElement().GetAttribute("ClassName") == "refundAmount"));
+        }
+
+        public static void PurchaseCountry()
+        {
+            var purchaseCountryDropDown = Driver.Instance.FindElement(By.Name("jurisdictions"));
+            purchaseCountryDropDown.SendKeys("AUT");
+        }
+
+        public static void PurchaseAmount(string purchaseAmountValue)
+        {
+            var purchaseAmountInput = Driver.Instance.FindElement(By.Name("refundInput"));
+            purchaseAmountInput.SendKeys(purchaseAmountValue);
+        }
+
+        public static void Calculate()
+        {
+            var calculateInput = Driver.Instance.FindElement(By.Name("submitRefund"));
+            calculateInput.Submit();
+        }
+
+        public static string RefundAmount()
+        {
+            var refundAmount = Driver.Instance.FindElement(By.ClassName("refundAmount"));
+            return refundAmount.Text;
+        }
+
+        public static string InvalidMessage()
+        {
+            var invalidMessage = Driver.Instance.FindElement(By.ClassName("message"));
+            return invalidMessage.Text;
+        }
+
+        public static void AppendRefund()
+        {
+            var appendRefund = Driver.Instance.FindElement((By.ClassName("appendRefund")));
+            appendRefund.Click();
+        }
+
+        public static string TotalRefundAmount()
+        {
+            var totalRefundAmount = Driver.Instance.FindElement(By.CssSelector(".refund.tblue"));
+            return totalRefundAmount.Text;
+        }
+    }
+}
